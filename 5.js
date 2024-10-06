31a7e2bf6727ce5cfe70832b5bae8c9b31e2c910
Ric Fred <54636736+ricfred71@users.noreply.github.com>
2024-10-06 10:05:26 -0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina 1</title>
-</head>
-<body>
-    <h1>Pagina 1</h1>
-    <a href="pagina2.html">Ir para Pagina 2</a>
-	</br>
-</br>
-<div id="links">
-<a href="pagina1.html">Pagina 1</a>
-<a href="pagina2.html">Pagina 2</a>
-<a href="pagina3.html">Pagina 3</a>
-<a href="pagina4.html">Pagina 4</a>
-<a href="pagina5.html">Pagina 5</a>
-</div>
-</br>
-</body>
-</html>
+console.log('Executando script da página 5');
+alert('Página 5 - Código externo carregado.');
+// Preencher o input com o texto selecionado
+document.querySelector('input[type="text"]').value = selectedText;
+// Clicar no botão
+document.querySelector('button').click();
