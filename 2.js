3cc53e95e105d3bcc1ed7c67066f48c944af78da
Ric Fred <54636736+ricfred71@users.noreply.github.com>
2024-10-06 10:07:56 -0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina 1</title>
-</head>
-<body>
-    <h1>Pagina 1</h1>
-    <a href="pagina2.html">Ir para Pagina 2</a>
-	</br>
-</br>
-<div id="links">
-<a href="pagina1.html">Pagina 1</a>
-<a href="pagina2.html">Pagina 2</a>
-<a href="pagina3.html">Pagina 3</a>
-<a href="pagina4.html">Pagina 4</a>
-<a href="pagina5.html">Pagina 5</a>
-</div>
-</br>
-</body>
-</html>
+console.log('Executando script da página 2');
+alert('Página 2 - Código externo carregado.');
+document.querySelector('a[href="pagina3.html"]').click();
